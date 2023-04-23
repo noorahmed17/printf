39480172b4a5187d33f2a086710c3da831d21f45
NawalAbdelmoniem <Nawal.mohamed0@gmail.com>
2023-04-23 13:57:57 -0700

    update

--- main.h
+++ main.h
@@ -45,10 +45,24 @@ int print_int(va_list types, char buffer[],
 int flags, int width, int precision, int size);
 int print_binary(va_list types, char buffer[],
 int flags, int width, int precision, int size);
+int print_unsigned(va_list types, char buffer[],
+int flags, int width, int precision, int size);
+int print_octal(va_list types, char buffer[],
+int flags, int width, int precision, int size);
+int print_hexadecimal(va_list types, char buffer[],
+int flags, int width, int precision, int size);
+int print_hexa_upper(va_list types, char buffer[],
+int flags, int width, int precision, int size);
+int print_hexa(va_list types, char map_to[],
+char buffer[], int flags, char flag_ch, int width, int precision, int size);
 int print_non_printable(va_list types, char buffer[],
 int flags, int width, int precision, int size);
 int print_pointer(va_list types, char buffer[],
 int flags, int width, int precision, int size);
+int get_flags(const char *format, int *i);
+int get_width(const char *format, int *i, va_list list);
+int get_precision(const char *format, int *i, va_list list);
+int get_size(const char *format, int *i);
 int print_reverse(va_list types, char buffer[],
 int flags, int width, int precision, int size);
 int print_rot13string(va_list types, char buffer[],
@@ -81,8 +95,6 @@ int print_octal(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 int print_unsigned(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
-
-
 /*flags*/
 #define F_MINUS 1
 #define F_PLUS 2
