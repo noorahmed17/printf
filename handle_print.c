f1983dcb0e4a73182138067a1fa4e31c0896f63b
NawalAbdelmoniem <Nawal.mohamed0@gmail.com>
2023-04-23 13:26:49 -0700

    update

--- handle_print.c
+++ handle_print.c
@@ -19,16 +19,16 @@ int handle_print(const char *fm, int *ind, va_list list, char buffer[]
 int i, unknow_len = 0, printed_chars = -1;
 fmt_t fmt_type[] =
 {
-['c', print_char, 's', print_string, '%', print_percent,
-'i', print_int, 'd', print_int, 'b', print_binary,
-'u', print_unsigned, 'o', print_octal, 'x', print_hexadecimal,
-'X', print_hexa_upper, 'p', print_pointer, 'S', print_non_printable,
-'r', print_reverse, 'R', print_rot13string, '\0', NULL];
-}
-for (i = 0; fmt_types[i].fmt != '\0'; i++)
-if (fmt[*ind] == fmt_types[i].fmt)
-return (fmt_types[i].fn(list, buffer, flags, wi, precision, size));
-if (fmt_types[i].fmt == '\0')
+{'c', print_char}, {'s', print_string}, {'%', print_percent},
+{'i', print_int}, {'d', print_int}, {'b', print_binary},
+{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
+{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
+{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
+};
+for (i = 0; fmt_type[i].fm != '\0'; i++)
+if (fm[*ind] == fmt_type[i].fm)
+return (fmt_type[i].fn(list, buffer, flags, wi, precision, size));
+if (fmt_type[i].fm == '\0')
 {
 if (fm[*ind] == '\0')
 return (-1);
