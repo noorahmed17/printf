27a5d0d2b7e3b4bdf797524851171ac6014d0993
NawalAbdelmoniem <125440893+NawalAbdelmoniem@users.noreply.github.com>
2023-04-25 16:59:27 +0200

    Update main.c

--- main.c
+++ main.c
@@ -40,13 +40,3 @@ int main(void)
     printf("Unknown:[/r]\n");
     return (0);
 }
-/**
- * main - Entry point
- *
- * Return: Always 0
- */
-/**int main(void)
-{
-    _printf("%S\n", "Best\nSchool");
-    return (0);
-}*/
