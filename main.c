6e00886800b1bb5dfd789eb4482cc994790974a4
NawalAbdelmoniem <125440893+NawalAbdelmoniem@users.noreply.github.com>
2023-04-25 15:49:51 +0200

    Update main.c

--- main.c
+++ main.c
@@ -39,6 +39,16 @@ int main(void)
     _printf("Len:[%d]\n", len);
     printf("Len:[%d]\n", len2);
     _printf("Unknown:[%r]\n");
-    printf("Unknown:[%r]\n");
+    printf("Unknown:[/r]\n");
+    return (0);
+}
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    _printf("%S\n", "Best\nSchool");
     return (0);
 }
