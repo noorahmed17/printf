0cf550716d1b943288958e9a5970810ff2436641
NawalAbdelmoniem <125440893+NawalAbdelmoniem@users.noreply.github.com>
2023-04-25 17:13:23 +0200

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -14,7 +14,7 @@ int _printf(const char *format, ...)
 
 	if (format == NULL)
 		return (-1);
-	va_start(args, format);
+	va_start(list, format);
 	for (i = 0; format && format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
@@ -28,11 +28,11 @@ int _printf(const char *format, ...)
 		{
 			print_buffer(buffer, &buff_end);
 			flags = get_flags(format, &i);
-			width = get_width(format, &i, args);
-			precision = get_precision(format, &i, args);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
 			size = get_size(format, &i);
 			++i;
-			printed = handle_print(format, &i, args, buffer, flags,
+			printed = handle_print(format, &i, list, buffer, flags,
 					width, precision, size);
 			if (printed == -1)
 				return (-1);
@@ -40,7 +40,7 @@ int _printf(const char *format, ...)
 		}
 	}
 	print_buffer(buffer, &buff_end);
-	va_end(args);
+	va_end(list);
 	return (printed_chars);
 }
 /**
