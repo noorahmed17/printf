149167a74b853fdb25a18d2eccb8cd284aa62759
NawalAbdelmoniem <125440893+NawalAbdelmoniem@users.noreply.github.com>
2023-04-25 14:33:51 +0200

    Update main.h

--- main.h
+++ main.h
@@ -55,10 +55,6 @@ int print_non_printable(va_list types, char buffer[],
 int flags, int width, int precision, int size);
 int print_pointer(va_list types, char buffer[],
 int flags, int width, int precision, int size);
-int get_flags(const char *format, int *i);
-int get_width(const char *format, int *i, va_list list);
-int get_precision(const char *format, int *i, va_list list);
-int get_size(const char *format, int *i);
 int print_reverse(va_list types, char buffer[],
 int flags, int width, int precision, int size);
 int print_rot13string(va_list types, char buffer[],
